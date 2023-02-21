af0e2d7ede30c018c25fa835c636fb84a1a32918
Deepsavaliya2507 <deepsavaliya2507@gmail.com>
2023-02-21 13:58:41 +0530

    done

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       work: 'Project Management Dashboard',
       complete: 'Done',
       salary: '50000',
-      joining : '4'
+      joining: '4'
     },
     {
       employee: 'radhey vaghasiya',
@@ -17,7 +17,7 @@ function App() {
       work: 'Quiz App',
       complete: 'Done',
       salary: '10000',
-      joining : '1'
+      joining: '1'
     },
     {
       employee: 'prabhat bhnderi',
@@ -25,7 +25,7 @@ function App() {
       work: 'Data structures',
       complete: 'Done',
       salary: '35000',
-      joining : '3'
+      joining: '3'
     },
     {
       employee: 'smit patel',
@@ -33,7 +33,7 @@ function App() {
       work: 'A Restaurant Menu',
       complete: 'In Progress',
       salary: '15000',
-      joining : '2'
+      joining: '2'
     },
     {
       employee: 'meet rakholiya',
@@ -41,57 +41,63 @@ function App() {
       work: 'admin panel',
       complete: 'In Progress',
       salary: '40000',
-      joining : '1'
+      joining: '1'
     },
   ]
 
-  const totalsalary = projectlist.reduce((acc, curr) => {
-    return acc + (curr.salary)
-  } , 0)
+  const total = projectlist.filter(j => j.complete == 'Done')
+
+  const totalsalary = total.reduce((acc, curr) => {
+    return acc + (curr.salary * curr.joining)
+  }, 0)
 
   return (
     <div className="App">
       <h1>Employee List</h1>
 
       <center>
-      <table border={1}>
-        <thead>
-          <tr>
-            <th>Id</th>
-            <th>Employee Name</th>
-            <th>Job</th>
-            <th>Currant Work</th>
-            <th>Complete Project</th>
-            <th>Salary</th>
-            <th>Joining Period</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            projectlist.filter(j => j.complete == 'Done').map((i, index) => {
-              return (
-                <tr>
-                  <td>{index + 1}</td>
-                  <td>{i?.employee}</td>
-                  <td>{i?.job_type}</td>
-                  <td>{i?.work}</td>
-                  <td>{i?.complete}</td>
-                  <td>{i?.salary}</td>
-                  <td>{i?.joining} Year</td>
-                </tr>
-              )
-            })
-          }
-          <tr>
-            <td colSpan={5}>
-              <b>Total :- </b>
-            </td>
-            <td>
-              {totalsalary}
-            </td>
-          </tr>
-        </tbody>
-      </table>
+        <table border={1}>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Employee Name</th>
+              <th>Job</th>
+              <th>Currant Work</th>
+              <th>Complete Project</th>
+              <th>Salary</th>
+              <th>Joining Period</th>
+              <th>Total Salary</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              projectlist.filter(j => j.complete == 'Done').map((i, index) => {
+                return (
+                  <tr>
+                    <td>{index + 1}</td>
+                    <td>{i?.employee}</td>
+                    <td>{i?.job_type}</td>
+                    <td>{i?.work}</td>
+                    <td>{i?.complete}</td>
+                    <td>{i?.salary}</td>
+                    <td>{i?.joining} Year</td>
+                    <td>{i?.salary * i?.joining}</td>
+                  </tr>
+                )
+              })
+            }
+            <tr>
+              <td colSpan={5}>
+                <b>Total :- </b>
+              </td>
+              <td colSpan={3}>
+                <center>
+                  {totalsalary}
+                </center>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </center>
 
     </div>
