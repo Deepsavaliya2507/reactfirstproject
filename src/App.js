31a38e7a0a598c028695fb36ba24e060e6224b52
Deepsavaliya2507 <deepsavaliya2507@gmail.com>
2023-02-21 13:00:52 +0530

    add employee list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const projectlist = [
+    {
+      employee: 'deep savaliya',
+      job_type: 'full stack developer',
+      work: 'Project Management Dashboard',
+      complete: 'Done',
+      salary: '50000',
+      joining : '4'
+    },
+    {
+      employee: 'radhey vaghasiya',
+      job_type: 'front end developer',
+      work: 'Quiz App',
+      complete: 'Done',
+      salary: '10000',
+      joining : '1'
+    },
+    {
+      employee: 'prabhat bhnderi',
+      job_type: 'back end developer',
+      work: 'Data structures',
+      complete: 'Done',
+      salary: '35000',
+      joining : '3'
+    },
+    {
+      employee: 'smit patel',
+      job_type: 'ui/ux designer',
+      work: 'A Restaurant Menu',
+      complete: 'In Progress',
+      salary: '15000',
+      joining : '2'
+    },
+    {
+      employee: 'meet rakholiya',
+      job_type: 'android developer',
+      work: 'admin panel',
+      complete: 'In Progress',
+      salary: '40000',
+      joining : '1'
+    },
+  ]
+
+  const totalsalary = projectlist.reduce((acc, curr) => {
+    return acc + (curr.salary)
+  } , 0)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Employee List</h1>
+
+      <center>
+      <table border={1}>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Employee Name</th>
+            <th>Job</th>
+            <th>Currant Work</th>
+            <th>Complete Project</th>
+            <th>Salary</th>
+            <th>Joining Period</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            projectlist.filter(j => j.complete == 'Done').map((i, index) => {
+              return (
+                <tr>
+                  <td>{index + 1}</td>
+                  <td>{i?.employee}</td>
+                  <td>{i?.job_type}</td>
+                  <td>{i?.work}</td>
+                  <td>{i?.complete}</td>
+                  <td>{i?.salary}</td>
+                  <td>{i?.joining} Year</td>
+                </tr>
+              )
+            })
+          }
+          <tr>
+            <td colSpan={5}>
+              <b>Total :- </b>
+            </td>
+            <td>
+              {totalsalary}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      </center>
+
     </div>
   );
 }
