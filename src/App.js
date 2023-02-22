dd77eeb61dda3376b7f57023f660f2ef0c9d6971
Deepsavaliya2507 <deepsavaliya2507@gmail.com>
2023-02-22 11:00:51 +0530

    last change

--- src/App.js
+++ src/App.js
@@ -8,60 +8,64 @@ function App() {
       job_type: 'full stack developer',
       work: 'Project Management Dashboard',
       complete: 'Done',
-      salary: '95000',
-      joining: '4'
+      salary: 95000,
+      joining: 4
     },
     {
       employee: 'Radhey Vaghasiya',
       job_type: 'front end developer',
       work: 'Quiz App',
       complete: 'Done',
-      salary: '10000',
-      joining: '1'
+      salary: 10000,
+      joining: 1
     },
     {
       employee: 'Prabhat Bhnderi',
       job_type: 'back end developer',
       work: 'Data structures',
       complete: 'Done',
-      salary: '35000',
-      joining: '3'
+      salary: 35000,
+      joining: 3
     },
     {
       employee: 'Smit Patel',
       job_type: 'ui/ux designer',
       work: 'A Restaurant Menu',
       complete: 'In Progress',
-      salary: '15000',
-      joining: '2'
+      salary: 15000,
+      joining: 2
     },
     {
       employee: 'Meet Rakholiya',
       job_type: 'android developer',
       work: 'admin panel',
       complete: 'In Progress',
-      salary: '40000',
-      joining: '1'
+      salary: 40000,
+      joining: 1
     },
     {
       employee: 'Aenil dhameliya',
       job_type: 'IOS developer',
       work: 'IOS Application',
       complete: 'Done',
-      salary: '43000',
-      joining: '2'
+      salary: 43000,
+      joining: 2
     },
     {
       employee: 'Ishita Naliyadra',
       job_type: 'backend developer',
       work: 'backend Application',
       complete: 'In Progress',
-      salary: '80000',
-      joining: '2'
+      salary: 80000,
+      joining: 2
     },
   ]
 
   const total = projectlist.filter(j => j.complete == 'Done')
+
+  const tatal = total.reduce((acc, curr) => {
+    return acc + curr.salary
+  }, 0)
 
   const totalsalary = total.reduce((acc, curr) => {
     return acc + (curr.salary * curr.joining)
@@ -106,7 +110,12 @@ function App() {
               <td colSpan={5}>
                 <b>Total :- </b>
               </td>
-              <td colSpan={3}>
+              <td >
+                <center>
+                  {tatal}
+                </center>
+              </td>
+              <td colSpan={2}>
                 <center>
                   {totalsalary}
                 </center>
